c4b6eb1b6aab2e321ccf4c922eb81dbbb5cef3eb
Jake LaPorte <49242583+jakelaporte@users.noreply.github.com>
2023-07-18 15:29:16 -0500

    test.py

--- test.py
+++ test.py
@@ -7,4 +7,10 @@ Created on Tue Jul 18 13:11:42 2023
 import skdh
 import streamlit as st
 
+file = st.file_uploader("Select the file to load:")
+if file is not None:
+  pipeline = skdh.Pipeline()
+  pipeline.add(skdh.io.ReadBin())
+  pipeline.add(skdh.preprocessing.CalibrateAccelerometer())
+  pipeline.add(skdh.gait.Gait())
 st.write("## Hello World")
